b1ca91f42d0199641d255969ea5161c99f218f43
Dipankar Maurya <dipankarmaurya5855@gmail.com>
2021-07-24 18:42:03 +0530

    find first occurence of an element in an array

---
+++ recursionInArray/returnFirstIndex.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int firstIndex(vector<int>array,int idx,int value){
+	
+	if(idx==array.size())
+	return -1;
+	int ans =firstIndex(array,idx+1,value);
+	if(array[idx]==value)
+    	return idx;
+    else
+	return ans;
+	
+}
+
+int main(){
+	int n;
+	cin>>n;
+	vector<int>v;
+	while(n--){
+		int x;
+		cin>>x;
+		v.push_back(x);
+	}
+	int value;
+	cin>>value;
+	int ans =firstIndex(v,0,value);
+	if(ans!=-1)
+	cout<<ans;
+	else cout<<"not found";
+}
